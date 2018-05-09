f7035ea78a5a3d02026746ab729aaf0b3788750b
nancydo7@gmail.com <nancydo7@gmail.com>
2018-05-09 20:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, countdown){
-  return `It is ${day}
+function holidayCountdown(holiday, days){
+  return `It is ${days} days until ${holiday}`
 }
